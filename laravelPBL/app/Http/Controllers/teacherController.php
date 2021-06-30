bef02e134addbdfb380ed6a765b2463db37fd35a
yutopc <yuto.tokunaga00@gmail.com>
2021-06-30 14:07:13 +0900

    遷移画面

---
+++ laravelPBL/app/Http/Controllers/teacherController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class teacherController extends Controller{
+    public function A003(){
+        return view('teacher.A003');
+    }
+    public function A005(){
+        return view('teacher.A005');
+    }
+    public function A006(){
+        return view('teacher.A006');
+    }
+}
---
+++ laravelPBL/resources/views/teacher/A005.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="./css/teacher.css">
+    </head>
+    <body>
+        <h1>詳細情報</h1>
+        <div id="left_table">
+            <div id="details_table">
+                <table border="1" id="details">
+                    <tr><th>氏名</th><td>test</td></tr>
+                    <tr><th>参加日</th><td>2021/6/15</td></tr>
+                    <tr><th>参加者NO</th><td>test</td></tr>
+                    <tr><th>年齢</th><td>18</td></tr>
+                    <tr><th>生年月日</th><td>2000/1/1</td></tr>
+                    <tr><th>学校</th><td>test</td></tr>
+                    <tr><th>学年</th><td>3</td></tr>
+                    <tr><th>メールアドレス</th><td>test</td></tr>
+                    <tr><th>住所</th><td>test</td></tr>
+                    <tr><th>郵便番号</th><td>test</td></tr>
+                    <tr><th>電話番号</th><td>test</td></tr>
+                    <tr><th>参加学科</th><td>情報スペシャリスト学科</td></tr>
+                    <tr><th>参加回数</th><td>test</td></tr>
+                    <tr><th>コース</th><td>test</td></tr>
+                    <tr><th>入試対象年度</th><td>test</td></tr>
+                    <tr><th>合否点数</th><td>test</td></tr>
+                </table>
+            </div>
+                <div id="right_memo">
+                    <p>メモ</p>
+                    <textarea></textarea>
+                </div>
+        </div>
+        <div class="bottom_button"><a href="A003" class="button_1">閉じる</a></div>
+    </body>
+</html>
