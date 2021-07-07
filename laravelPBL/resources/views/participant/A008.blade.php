f3fb5c03d2ab896e2d2d8d06009f1f42000418a1
yusuke29 <73320745+yusuke29@users.noreply.github.com>
2021-07-07 12:28:54 +0900

    画面

--- laravelPBL/resources/views/participant/A008.blade.php
+++ laravelPBL/resources/views/participant/A008.blade.php
@@ -8,9 +8,9 @@
         <p>岡山情報ビジネス学院<br>オープンキャンパス参加者情報入力フォーム</p>
     </header>
     <body>
-        <a href="/team8.systemsUNO/laravelPBL/public/A007"><p>＜戻る</p></a>
-        <u><p style="margin-left: 70px;margin-top: 100px;">２週間以内に県外へ移動しましたか？</p></u>
-        <div id="yn">
+    <input type="button" onclick="history.back()" class="back" value="☜前画面へ戻る">
+        <u><p style="margin-left: 45px;margin-top: 100px;font-size: 20px;">２週間以内に県外へ移動しましたか？</p></u>
+        <div class="yn">
             <a href="/team8.systemsUNO/laravelPBL/public/A009"><button type="button" class="botton_1" id="move_yes">はい</button></a>
             <a href="/team8.systemsUNO/laravelPBL/public/A010"><button type="button" class="botton_2" id="move_no">いいえ</button></a>
         </div>
--- laravelPBL/resources/views/participant/A010.blade.php
+++ laravelPBL/resources/views/participant/A010.blade.php
@@ -8,9 +8,9 @@
         <p>岡山情報ビジネス学院<br>オープンキャンパス参加者情報入力フォーム</p>
     </header>
     <body>
-        <a href="/team8.systemsUNO/laravelPBL/public/A008"><p>＜戻る</p></a>
-        <u><p style="margin-left: 40px;margin-top: 100px;">オープンキャンパスの参加は初めてですか？</p></u>
-        <div id="yn">
+    <input type="button" onclick="history.back()" class="back" value="☜前画面へ戻る">
+        <u><p style="margin-left: 20px;margin-top: 100px;font-size: 20px;">オープンキャンパスの参加は初めてですか？</p></u>
+        <div class="yn">
         <a href="/team8.systemsUNO/laravelPBL/public/A012"><button type="button" class="botton_1" id="join_yes">はい</button></a>
         <a href="/team8.systemsUNO/laravelPBL/public/A011"><button type="button" class="botton_2" id="join_no">いいえ</button></a>
         </div>
